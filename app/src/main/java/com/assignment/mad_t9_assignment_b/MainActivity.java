afa8e6c3190fe4ea48356366f625db86f72fa9a1
Thasneem Abdul Saleem Mohamed Thasneem <abdulsal@student.curtin.edu.au>
2022-10-14 12:19:03 +0530

    Created a Empty Project with min sdk 28

---
+++ app/src/main/java/com/assignment/mad_t9_assignment_b/MainActivity.java
@@ -0,0 +1,17 @@
+package com.assignment.mad_t9_assignment_b;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
